ea89356ab10e9358e0eb15fea44f32cdc3a627d3
c <thierrybanel@free.fr>
2024-12-23 19:55:56 +0100

    new regression test for double arrows ↕ ↔

---
+++ tests/bench15.el
@@ -0,0 +1,12 @@
+(uniline-bench
+"<return> <down> <down> <down> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <kp-subtract> <up> <up> <right> <right> <right> <insert> a <right> <insert> a a <right> <insert> a a a <right> <insert> a a a a <right> <insert> a a a a a <right> <insert> a a a a a a <right> <right> <right> <down> <insert> A <down> <insert> A A <down> <insert> a <down> <insert> A A A A <down> <insert> a S-<right> <down> <left> <left> <left> <insert> A <left> <home> <return> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <right> <left> <insert> A S-<up> <left> <insert> a <left> <left> <kp-subtract> <left> <left> <insert> A A A A <left> <left> <up> <insert> a a a S-<left> <up> <insert> a S-<right> <up> <insert> a a a <up> <insert> a a a a a <up> <insert> a a a a a a S-<right> <right> <right> <right> <return> <insert> a <right> <right> <right> <up> <insert> S-<down> <down> <down> <down> <right> <right> <right> <right> <right> <insert> S-<right>"
+"\
+ 
+          ╭──▷▶→▿▸↔──╮
+          ↔──▷       ↕
+          ▴          ▾
+          ↑          ▷
+          ▷          ↓
+          ←          ▷
+          ╰─←─╴ ◁↕↔──╯
+")
