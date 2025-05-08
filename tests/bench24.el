8bd832729c53407d9567061549d46c059696d02d
c <thierrybanel@free.fr>
2025-05-08 09:32:29 +0200

    add unit-tests for transparent rectangle yanking

---
+++ tests/bench24.el
@@ -0,0 +1,32 @@
+;;; uniline.el --- Draw lines, boxes, & arrows with the keyboard  -*- coding:utf-8; lexical-binding: t; -*-
+
+;; Copyright (C) 2024-2025  Thierry Banel
+
+;; Author: Thierry Banel tbanelwebmin at free dot fr
+;; Version: 1.0
+;; URL: https://github.com/tbanel/uniline
+
+;; Uniline is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Uniline is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(uniline-bench
+"<return> <down> <down> <down> <right> <right> <right> <right> <right> u u u u u u <down> <left> <left> <left> <left> u u u u u u # <down> <down> <left> <left> <down> <left> <left> <left> <left> <left> <left> <left> <up> <left> <left> <left> C-_ C-_ C-_ C-_ C-_ C-_ <down> <down> <return> <up> <up> <up> <up> <kp-subtract> <left> <left> <left> <down> <down> <down> <down> <right> <right> <right> <right> <right> <right> <up> <up> <up> <up> <left> <left> <left> <return> <left> <left> <left> <left> S-<down> S-<down> S-<down> S-<down> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> <insert> <right> <right> s <kp-add> <down> <down> s <kp-3> <right> s <kp-3> <down> C-_ C-_ C-_ C-_ C-_ C-_ C-_ C-_ i SPC C-_ <return> i"
+"\
+ 
+ 
+      ╭─────╮
+     uuuuuu │
+      │uuuuuu▖
+      │  ▗▄▄▛▘
+      ╰──▐──╯ i
+")
---
+++ tests/bench25.el
@@ -0,0 +1,31 @@
+;;; uniline.el --- Draw lines, boxes, & arrows with the keyboard  -*- coding:utf-8; lexical-binding: t; -*-
+
+;; Copyright (C) 2024-2025  Thierry Banel
+
+;; Author: Thierry Banel tbanelwebmin at free dot fr
+;; Version: 1.0
+;; URL: https://github.com/tbanel/uniline
+
+;; Uniline is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Uniline is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(uniline-bench
+"<return> <down> <right> <right> a a a a a <down> <left> a a a a a <down> <left> <left> <left> <left> <left> <left> <left> t t t t t <up> <up> <right> <right> <right> <right> <right> S-<down> S-<down> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> S-<right> <insert> <kp-subtract> r <return> <return> <down> <right> <right> <right> <right> <insert> o o o <up> <left> <left> <left> <left> <left> <right> C-SPC <down> <down> <right> <right> <right> <right> <right> <right> <right> <insert> c <left> <left> <left> <left> <left> <up> <insert> y <return> <return> <left> <left> <left> <left> <left> <left> <left> <left> <left> <left> <left> <left> <up> <left> <up> C-SPC <insert> y <return> <down> <down> <down> <right> <right> <right> <up> C-SPC <insert> y <return>"
+"\
+   ╭─────╮
+  a│aaa• │    ╭─────╮
+   ╰──╭─────╮ │ ╭─────╮
+    tt│tt • │ ╰─│──╴• │
+      ╰─────╯   ╰─────╯
+                
+")
