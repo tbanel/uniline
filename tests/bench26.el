e11138652429c6a3d629dd51b6ef138b563c56e9
c <thierrybanel@free.fr>
2025-10-30 12:36:09 +0100

    fix bench26.el

--- tests/bench26.el
+++ tests/bench26.el
@@ -19,8 +19,18 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;; Test ASCII → Uniline base-line
+
 (uniline-bench
-"<return> <down> C-<insert> SPC SPC SPC SPC SPC SPC SPC SPC SPC <kp-add> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-add> SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC <kp-add> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-add> <return> <left> <left> <left> <left> <left> <left> / <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> / SPC R e c t a n l <backspace> g l e SPC 1 SPC | - - - - - - - - - - - - - + SPC R e c t a n g l e SPC 2 SPC SPC ` <backspace> | <return> | SPC SPC SPC SPC SPC | SPC SPC < s i n ^ g l e > SPC SPC | SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC | SPC \" q o <backspace> u o t e \" SPC SPC SPC SPC SPC SPC + - <return> | SPC SPC SPC SPC SPC \\ <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> / SPC SPC SPC / <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> \\ SPC SPC SPC + <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-add> <return> | SPC SPC SPC SPC SPC \\ <kp-subtract> <kp-subtract> <kp-add> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-add> <kp-subtract> <kp-subtract> / SPC SPC SPC SPC <insert> <backspace> | SPC SPC SPC SPC SPC <left> <left> <left> <left> v a v SPC | SPC SPC SPC + <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> \\ <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> / <kp-subtract> <kp-subtract> <kp-subtract> + <return> \\ > <kp-subtract> <kp-subtract> \\ SPC SPC SPC SPC | SPC SPC SPC SPC SPC SPC SPC | SPC SPC SPC SPC SPC SPC \\ <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> <kp-subtract> / SPC SPC SPC SPC SPC SPC SPC SPC SPC <backspace> | SPC SPC SPC SPC SPC ` <backspace> | <return> SPC SPC SPC SPC v SPC SPC SPC SPC \\ SPC SPC <backspace> <backspace> <kp-subtract> <kp-subtract> > - - - - / SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC SPC + SPC SPC <backspace> <backspace> <kp-subtract> <kp-subtract> < <kp-subtract> <kp-subtract> + SPC SPC SPC SPC SPC SPC C-<insert> C-SPC M-< <down> <down> <up> <insert> s <kp-0> <return> <return>"
+"<return> <down> C-<insert>
+ 9*SPC <kp-add> 13*<kp-subtract> <kp-add> 13*SPC <kp-add> 14*<kp-subtract> <kp-add> <return> C-a
+ 3*SPC / 5*<kp-subtract> / SPC R e c t a n l <backspace> g l e SPC 1 SPC | 13*- + SPC R e c t a n g l e SPC 2 2*SPC ` <backspace> | <return> C-a
+3*SPC | 5*SPC | 2*SPC < s i n ^ g l e > 2*SPC | 13*SPC | SPC \" q o <backspace> u o t e \" 6*SPC + - <return> C-a
+ 3*SPC | 5*SPC \\ 13*<kp-subtract> / 3*SPC / 5*<kp-subtract> \\ 3*SPC + 14*<kp-subtract> <kp-add> <return> C-a
+ 3*SPC | 5*SPC \\ 2*<kp-subtract> <kp-add> 7*<kp-subtract> <kp-add> 2*<kp-subtract> / 4*SPC <insert> <backspace> | 5*SPC 4*<left> v a v SPC | 3*SPC + 4*<kp-subtract> \\ 5*<kp-subtract> / 3*<kp-subtract> + <return> C-a
+ 3*SPC \\ > 2*<kp-subtract> \\ 4*SPC | 7*SPC | 6*SPC \\ 5*<kp-subtract> / 9*SPC <backspace> | 5*SPC ` <backspace> | <return> C-a
+ 7*SPC v 4*SPC \\ 2*SPC 2*<backspace> 2*<kp-subtract> > 4*- / 21*SPC + 2*SPC 2*<backspace> 2*<kp-subtract> < 2*<kp-subtract> + 6*SPC
+ C-<insert> C-SPC M-< 2*<down> <up> <insert> s <kp-0> 2*<return>"
 "\
  
          ╭─────────────╮             ╭──────────────╮  
